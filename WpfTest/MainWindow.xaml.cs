d95cdb0bc6363190b070d012de93a63bbe3bf9e3
hellcat28 <dltrongluat@gmail.com>
2019-01-08 11:27:43 +0700

    Add project files.

---
+++ WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        public class Question
+        {
+            public int id { get; set; }
+            public string question { get; set; }
+            public string published_at { get; set; }
+        }
+        private void Go_Clickk(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient("https://polls.apiblueprint.org/");
+           // client.Authenticator = new HttpBasicAuthenticator("admin","0123456789");
+      
+            var request = new RestRequest("questions", Method.GET);
+
+            //IRestResponse response = client.Execute(request);
+            IRestResponse response = client.Execute<Question>(request);
+            //var content = response.Content;
+            //textBox.Text = content;
+            var o = JsonConvert.DeserializeObject<Question>(json);
+            
+        }
+    }
+}
---
+++ WpfTest/RestClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+namespace WpfTest
+{
+    //public class UserLogin
+    //{
+    //    public string Token { get; set; }
+    //    public string Secret { get; set; }
+    //}
+    //public UserLogin Login ()
+    //{
+
+     
+
+   
+
+      
+    //}
+}
