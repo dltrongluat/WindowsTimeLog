e777c87e70f7d1cfc2aa44287859babf8a55d9c4
Luat Dang <dltrongluat@gmail.com>
2019-02-23 16:10:37 +0700

    test with notification when log time auto

---
+++ WpfApp2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace WpfApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier();
+           
+            popup.TitleText = "wtssdsadsadf";
+            popup.ContentText = "hahsadasda";
+            
+            popup.Popup();//show
+        }
+    }
+}
