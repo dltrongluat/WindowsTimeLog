d65db13b60a0a5f6105af7b77d3b584e77164d04
Luat Dang <dltrongluat@gmail.com>
2019-03-25 19:05:34 +0700

    add delete function for add new time entry activites

--- WpfTest/Setting_Page.xaml.cs
+++ WpfTest/Setting_Page.xaml.cs
@@ -146,7 +146,36 @@ namespace WpfTest
 
 
         }
-
+        public void Update_Click(object sender, RoutedEventArgs e)
+        {
+            string directory = System.IO.Directory.GetParent(System.IO.Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString();
+            var file = Path.Combine(directory, "TE_Activities.txt");
+            int Line = 2;
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                int Countup = 0;
+                while (!sr.EndOfStream)
+                {
+                    Countup++;
+                    if (Countup != Line)
+                    {
+                        using (StringWriter sw = new StringWriter(sb))
+                        {
+                            sw.WriteLine(sr.ReadLine());
+                        }
+                    }
+                    else
+                    {
+                        sr.ReadLine();
+                    }
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                sw.Write(sb.ToString());
+            }
+        }
         private void WriteFile()
         {
             List<App.TE_Settingg> Setting = (App.Current as App).elements;
